c8839657643ec8ed7322cb46cb4159b5268ab03f
Su-maan <shalumanoj960@gmail.com>
2025-10-19 17:33:20 +0530

    Temporarily disable console silencer to fix 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './i18n';
-import './utils/consoleSilencer';
+// import './utils/consoleSilencer'; // Temporarily disabled
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
